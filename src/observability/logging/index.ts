7cae2458a49e1b3a2dffd0d3c93ce7ce91658845
shafeeque <shafeeqsha06@gmail.com>
2026-07-16 11:51:54 +0530

    feat(logger): update logger with allowing caller to inject config though factor method and exports shutdown hook

--- src/observability/logging/index.ts
+++ src/observability/logging/index.ts
@@ -2,4 +2,4 @@ export * from "./types";
 export { ILogger } from "./logger.interface";
 export * from "./logger.service";
 export * from "./logger.factory";
-export { createLogger } from "./logger.factory";
+export { createLogger, shutdownLogger } from "./logger.factory";
