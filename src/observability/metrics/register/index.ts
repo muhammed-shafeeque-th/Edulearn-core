a583c8b307bc30209a9d2cfe94a404fb17ca5d95
shafeeque <shafeeqsha06@gmail.com>
2026-07-17 12:02:25 +0530

    feat(metrics): update metrics server, inject metrics registy to expose metrics to outside

---
+++ src/observability/metrics/register/index.ts
@@ -0,0 +1,4 @@
+export type { MetricsEngine } from "./metrics.engine";
+export { MetricFactory } from "./metric.factory";
+export { MetricService } from "./prom-metric.service";
+export type { MetricRegistry } from "./registry";
