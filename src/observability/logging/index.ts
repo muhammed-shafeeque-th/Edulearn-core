5704a9c8440878d0385c45f04b03da3d16553141
shafeeque <shafeeqsha06@gmail.com>
2026-07-17 16:38:10 +0530

    feat(logger): update observability logger services and parts, expose only custom types rather provider types, improve loose coupling

--- src/observability/logging/index.ts
+++ src/observability/logging/index.ts
@@ -1,4 +1,5 @@
 export * from "./types";
 export { ILogger } from "./logger.interface";
+export * from "./logger.config";
 export { LoggerService } from "./logger.service";
 export { createLogger, shutdownLogger } from "./bootstrap/logger.factory";
